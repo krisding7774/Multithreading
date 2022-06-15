cc9b86f9a2b10f4e467f9af108db8c99406a34ef
ricky <krisding7774@gmail.com>
2022-06-15 17:09:22 +0800

    2022/06/15 17:09 p.m

--- src/com/DuplicateScopusAdvanced.java
+++ src/com/DuplicateScopusAdvanced.java
@@ -13,7 +13,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSON;
@@ -23,62 +26,67 @@ public class DuplicateScopusAdvanced {
 
 	private static final String root = "D:\\workspace\\change\\example\\storehouse";
 
-	public static List<String> replaceAPI() throws IOException {
+	public static Map<String, String> replaceAPI() throws IOException {
 //		需將此API的isbns號更改為資料庫內的isbns號
 		String api = "http://127.0.0.1:8080/example/API?isbns=[isbn]&key=g72e6-cone2-h7sc6-j50mr-mpyj3";
 
-		List<String> list = new ArrayList<>();
+		Map<String, String> map = new HashMap<>();
 //		讀取路徑位置以及位置內的網址檔案
 		List<String> urlList = Files.readAllLines(Paths.get(root, "url.txt"));
+		List<String> mIdList = Files.readAllLines(Paths.get(root, "mId.txt"));
 //		將不需要的訊息替換成空白，只留下需要的isbns號
 		List<String> isbnList = urlList.stream().map(isbn -> {
 			return isbn.replace("http://static.example.tw/image/book/", "").replace(" ", "").replace("-", "")
 					.replace("b平裝 ", "").replace("$平裝", "").replace(":", "").replace("/large", "");
 		}).collect(Collectors.toList());
 
+//		利用迴圈將api的[isbn]換成實際存在的isbn號
 		for (int i = 0; i < urlList.size(); i++) {
-			list.add(api.replace("[isbn]", isbnList.get(i)));
+			map.put(mIdList.get(i), api.replace("[isbn]", isbnList.get(i)));
 		}
-		return list;
+
+		return map;
 	}
 
 	public static void main(String[] args) throws IOException {
 //		產生的SQL語法
 		String sql = "UPDATE example SET url = [isbn] WHERE mId = [mId];";
+		String sql2 = "UPDATE example SET url = null WHERE mId = [mId];";
 
-//		讀取路徑位置以及位置內的網址檔案
-		List<String> mIdList = Files.readAllLines(Paths.get(root, "M_ID.txt"));
-		List<String> isbnList = new ArrayList<>();
-		List<String> list = new ArrayList<>();
-//		純粹計算此程式跑了第幾次
-		int count = 1;
+		try {
+			List<String> list = new ArrayList<>();
+//			純粹計算此程式跑了第幾次
+			int count = 1;
 
-		for (String api : replaceAPI()) {
-//			由於需要查看網站狀態，故使用URLConnection來訪問URL網址
-			URLConnection conn = new URL(api).openConnection();
-//			透過getHeaderField查看HTTP的連線狀況，如果是404、405、500則忽略不會進行讀寫
-			if (conn.getHeaderField(0).equals("HTTP/1.1 200 OK")) {
-				BufferedReader bufferedRead = new BufferedReader(new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
-//				將讀出來的json格式轉換成obj，方便獲取img資料
-				JSONObject obj = JSON.parseObject(bufferedRead.readLine());
-				isbnList.add(obj.getString("img"));
-				System.out.println("API Running... NO." + count++);
+			for (Entry<String, String> api : replaceAPI().entrySet()) {
+//				由於需要查看網站狀態，故使用URLConnection來訪問URL網址
+				URLConnection conn = new URL(api.getValue()).openConnection();
+
+				String result = null;
+
+//				透過getHeaderField查看HTTP的連線狀況，如果是404、405、500則忽略不會進行讀寫
+				if (conn.getHeaderField(0).equals("HTTP/1.1 200 OK")) {
+					BufferedReader bufferedRead = new BufferedReader(
+							new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
+//					將讀出來的json格式轉換成obj，方便獲取img資料
+					JSONObject obj = JSON.parseObject(bufferedRead.readLine());
+					result = sql.replace("[isbn]", obj.getString("img")).replace("[mid]", api.getKey());
+					System.out.println("API No." + +count++ + " is running...");
+				} else {
+					result = sql2.replace("[mid]", api.getKey());
+				}
+				list.add(result);
 			}
-		}
-//		把計數器重置繼續算跑幾次	
-		count = 1;
 
-//		將SQL語法替換成想要的格式
-		for (int i = 0; i < isbnList.size(); i++) {
-			list.add(sql.replace("[isbn]", isbnList.get(i)).replace("[mid]", mIdList.get(i)));
-			System.out.println("SQL Running... NO." + count++);
-		}
+//			寫入路徑以及檔案名稱
+			Path path = Paths.get(root, "result.sql");
+//			編碼格式為UTF-8
+			Charset cs = Charset.forName("UTF-8");
+//			在指定路徑輸出修改後網址，並設定編碼格式為UTF-8
+			Files.write(path, list, cs, WRITE, CREATE);
 
-//		寫入路徑以及檔案名稱
-		Path path = Paths.get(root, "resoult.sql");
-//		編碼格式為UTF-8
-		Charset cs = Charset.forName("UTF-8");
-//		在指定路徑輸出修改後網址，並設定編碼格式為UTF-8
-		Files.write(path, list, cs, WRITE, CREATE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/DuplicateScopusThread.java
+++ src/com/DuplicateScopusThread.java
@@ -13,7 +13,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSON;
@@ -31,13 +34,14 @@ public class DuplicateScopusThread extends Thread {
 		this.url = url;
 	}
 
-	public List<String> replaceAPI() throws IOException {
+	public Map<String, String> replaceAPI() throws IOException {
 //		需將此API的isbns號更改為資料庫內的isbns號
 		String api = "http://127.0.0.1:8080/example/API?isbns=[isbn]&key=g72e6-cone2-h7sc6-j50mr-mpyj3";
 
-		List<String> list = new ArrayList<>();
+		Map<String, String> map = new HashMap<>();
 //		讀取路徑位置以及位置內的網址檔案
 		List<String> urlList = Files.readAllLines(Paths.get(this.root, this.url));
+		List<String> mIdList = Files.readAllLines(Paths.get(this.root, this.mId));
 //		將不需要的訊息替換成空白，只留下需要的isbns號
 		List<String> isbnList = urlList.stream().map(isbn -> {
 			return isbn.replace("http://static.example.tw/image/book/", "").replace(" ", "").replace("-", "")
@@ -46,45 +50,43 @@ public class DuplicateScopusThread extends Thread {
 
 //		利用迴圈將api的[isbn]換成實際存在的isbn號
 		for (int i = 0; i < urlList.size(); i++) {
-			list.add(api.replace("[isbn]", isbnList.get(i)));
+			map.put(mIdList.get(i), api.replace("[isbn]", isbnList.get(i)));
 		}
 
-		return list;
+		return map;
 	}
 
 	@Override
 	public void run() {
 //		產生的SQL語法
 		String sql = "UPDATE example SET url = [isbn] WHERE mId = [mId];";
+		String sql2 = "UPDATE example SET url = null WHERE mId = [mId];";
 
 		try {
-//			讀取路徑位置以及位置內的網址檔案
-			List<String> mIdList = Files.readAllLines(Paths.get(this.root, this.mId));
-			List<String> isbnList = new ArrayList<>();
 			List<String> list = new ArrayList<>();
 //			純粹計算此程式跑了第幾次
 			int count = 1;
 
-			for (String api : replaceAPI()) {
+			for (Entry<String, String> api : replaceAPI().entrySet()) {
 //				由於需要查看網站狀態，故使用URLConnection來訪問URL網址
-				URLConnection conn = new URL(api).openConnection();
+				URLConnection conn = new URL(api.getValue()).openConnection();
+
+				String result = null;
+
 //				透過getHeaderField查看HTTP的連線狀況，如果是404、405、500則忽略不會進行讀寫
 				if (conn.getHeaderField(0).equals("HTTP/1.1 200 OK")) {
-					BufferedReader bufferedRead = new BufferedReader(new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
+					BufferedReader bufferedRead = new BufferedReader(
+							new InputStreamReader(conn.getInputStream(), Charset.forName("UTF-8")));
 //					將讀出來的json格式轉換成obj，方便獲取img資料
 					JSONObject obj = JSON.parseObject(bufferedRead.readLine());
-					isbnList.add(obj.getString("img"));
+					result = sql.replace("[isbn]", obj.getString("img")).replace("[mid]", api.getKey());
 					System.out.println("API No." + this.thread + " is running... frequency：" + count++);
+				} else {
+					result = sql2.replace("[mid]", api.getKey());
 				}
+				list.add(result);
 			}
-//			把計數器重置繼續算跑幾次	
-			count = 1;
 
-//			將SQL語法替換成想要的格式
-			for (int i = 0; i < isbnList.size(); i++) {
-				list.add(sql.replace("[isbn]", isbnList.get(i)).replace("[mId]", mIdList.get(i)));
-				System.out.println("SQL No." + this.thread + "is running... frequency：" + count++);
-			}
 //			寫入路徑以及檔案名稱
 			Path path = Paths.get(this.root, this.thread + ".sql");
 //			編碼格式為UTF-8
